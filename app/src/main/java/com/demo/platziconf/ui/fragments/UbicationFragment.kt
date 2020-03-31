3e6e72389043cc49a2ac2481c1cc70813b6f7aed
iaSalazar <ia.salazar@uniandes.edu.co>
2020-03-31 18:38:37 -0500

    diseño y parte de la navegación

--- app/src/main/java/com/demo/platziconf/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/demo/platziconf/ui/fragments/UbicationFragment.kt
@@ -18,7 +18,7 @@ class UbicationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ubication2, container, false)
+        return inflater.inflate(R.layout.fragment_ubication, container, false)
     }
 
 }
