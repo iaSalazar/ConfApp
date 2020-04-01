68f9b1772b11f62103205c8e1d7287e641028bb5
iaSalazar <ia.salazar@uniandes.edu.co>
2020-04-01 18:58:20 -0500

    JSON issues

---
+++ app/src/main/java/com/demo/platziconf/model/Ubication.kt
@@ -0,0 +1,10 @@
+package com.demo.platziconf.model
+
+class Ubication {
+    val name = ""
+    val address = ""
+    val latitude = ""
+    val phone = ""
+    val website = ""
+    val photo = ""
+}
