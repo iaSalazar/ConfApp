502f9a4323e1f1e699e77744a1bbead74c22cd50
iaSalazar <ia.salazar@uniandes.edu.co>
2020-04-08 15:26:43 -0500

    splash screen and google maps.

--- app/src/main/java/com/demo/platziconf/view/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/demo/platziconf/view/ui/activities/SplashScreenActivity.kt
@@ -1,13 +1,56 @@
 package com.demo.platziconf.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.demo.platziconf.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val animation = AnimationUtils.loadAnimation(this,R.anim.animation)
+        ivLogoPlatzi.startAnimation(animation)
+
+        val intent = Intent(this,MainActivity::class.java)
+        animation.setAnimationListener(object : Animation.AnimationListener{
+            /**
+             *
+             * Notifies the repetition of the animation.
+             *
+             * @param animation The animation which was repeated.
+             */
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+            /**
+             *
+             * Notifies the end of the animation. This callback is not invoked
+             * for animations with repeat count set to INFINITE.
+             *
+             * @param animation The animation which reached its end.
+             */
+            override fun onAnimationEnd(animation: Animation?) {
+
+                startActivity(intent)
+                finish()
+            }
+
+            /**
+             *
+             * Notifies the start of the animation.
+             *
+             * @param animation The started animation.
+             */
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+        })
     }
 }
--- app/src/main/java/com/demo/platziconf/view/ui/fragments/UbicationDetailDialogFragment.kt
+++ app/src/main/java/com/demo/platziconf/view/ui/fragments/UbicationDetailDialogFragment.kt
@@ -1,17 +1,29 @@
 package com.demo.platziconf.view.ui.fragments
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 
 import com.demo.platziconf.R
+import com.demo.platziconf.model.Ubication
+import kotlinx.android.synthetic.main.fragment_ubication_detail_dialog.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class UbicationDetailDialogFragment : Fragment() {
+class UbicationDetailDialogFragment : DialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL,R.style.fullScreenDialogStyle)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,4 +33,40 @@ class UbicationDetailDialogFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_ubication_detail_dialog, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        super.onViewCreated(view, savedInstanceState)
+        toolbarUbication.navigationIcon = ContextCompat.getDrawable(view.context, R.drawable.close)
+        toolbarUbication.setTitleTextColor(Color.WHITE)
+        toolbarUbication.setNavigationOnClickListener {
+            dismiss()
+        }
+
+        val ubication = Ubication()
+
+        toolbarUbication.title = ubication.name
+        tvUbicationName.text = ubication.name
+        tvUbicationAdress.text = ubication.address
+        tvUbicationPhone.text = ubication.phone
+        tvUbicationWeb.text = ubication.website
+
+        llTelefonoLugar.setOnClickListener{
+
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:${ubication.phone}")
+            }
+        }
+        llWeb.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(ubication.website)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT)
+    }
 }
